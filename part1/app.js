64dc20c8716fa93a69e7f1d7b159866b10a751bc
Lachlan Hennessy <104713632+Hennessy05@users.noreply.github.com>
2025-06-20 04:54:24 +0000

    [automatic commit] 2025-06-20 04:54:24 UTC 20.227.140.177

--- part1/app.js
+++ part1/app.js
@@ -10,6 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 let db;
 
 (async () => {
