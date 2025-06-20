d489dc2d97e7d4dfa3d7a21c6d82de6a71748217
Lachlan Hennessy <104713632+Hennessy05@users.noreply.github.com>
2025-06-20 08:12:02 +0000

    [automatic commit] 2025-06-20 08:12:02 UTC 20.227.141.209

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,7 +49,7 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
-    // this Save user to session
+    // this saves the user to an associated session
     req.session.user = rows[0];
 
     res.json({ message: 'Login successful', user: rows[0] });
